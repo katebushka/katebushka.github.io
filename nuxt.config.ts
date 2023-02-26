52aec3b48dd087ae882ccbcd08ec37856493c7f7
kater <lrikil@mail.ru>
2023-02-26 14:04:24 +0300

    upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: [
-        '~/assets/css/main.css',
-    ],
-    app: {
-        head: {
-          charset: 'utf-8',
-          viewport: 'width=500, initial-scale=1',
-          link: [
-            { rel: 'icon', href: '/favicon.ico' },
-            {
-              href: 'https://fonts.googleapis.com/css2?family=Orbitron&display=swap', rel: 'stylesheet'
-            },
-            {
-            href: "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap", rel: "stylesheet"
-            }
-          ],
-        }
-      },
-    
+        '@/assets/css/main.css',
+    ],    
 })
