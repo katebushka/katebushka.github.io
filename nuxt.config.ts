707e6cc1250f19f55931ca1f195f709cf26d8656
kater <lrikil@mail.ru>
2023-02-26 14:57:10 +0300

    upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['assets/css/main.css'] 
+    css: [
+        "/katebushka.github.io/assets/css/main.css"
+        // '@/assets/css/main.css',
+    ],
+    app: {
+        head: {
+          charset: 'utf-8',
+          viewport: 'width=500, initial-scale=1',
+          link: [
+            { rel: 'icon', href: '/favicon.ico' },
+            {
+              href: 'https://fonts.googleapis.com/css2?family=Orbitron&display=swap', rel: 'stylesheet'
+            },
+            {
+            href: "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap", rel: "stylesheet"
+            }
+          ],
+        }
+      },
+    
 })
