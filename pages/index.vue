f373bfed6b55d7872c8b244bdefce5179f90216e
kater <lrikil@mail.ru>
2023-03-04 00:59:25 +0300

    move fn

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,74 @@ const { data: items } = await useFetch(`/api/skills/all`)
 const skills = computed(() => {
   return items.value;
 });
+
+onMounted(function () {
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+
+function onSkill(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      let w = change.target.getAttribute('data-width-line')
+      change.target.style.width = w
+      console.log(w);
+      change.target.animate(
+        [
+          { width: '0%' },
+          { width: w }
+        ],
+        {
+          // timing options
+          duration: 2000,
+          iterations: 1,
+        }
+      )
+    }
+  })
+};
+let options1 = { threshold: [0.5] };
+let observer1 = new IntersectionObserver(onSkill, options1);
+
+let elements = document.querySelectorAll('.tracking');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+let text = document.querySelectorAll('.text');
+for (let txt of text) {
+  observer.observe(txt);
+}
+
+let skills = document.querySelectorAll('.skills');
+for (let skill of skills) {
+  observer1.observe(skill);
+}
+
+
+const myButton = document.getElementById("btn");
+
+window.onscroll = function () {
+  if (window.pageYOffset >= 800) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+};
+
+myButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
+
+});
+
 </script>
 
 <template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -4,6 +4,6 @@
 
 <script setup>
 definePageMeta({
-    layout: 'project'
+    layout: 'project',
 })
 </script>
